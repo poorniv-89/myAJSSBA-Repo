96abe37e78a1c8ddaeb60a4f312899f9277a1d06
poorniv-89 <poorniv.89@gmail.com>
2025-04-11 00:43:26 -0600

    added comments to index script

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,28 @@
+
+//importing modules 
 import * as recipeModule from '../scripts/recipe.js';
 import * as searchModule from '../scripts/searchRecipe.js'
+
+//get the dom elements and cache into variables
 const searchInputEl = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
-
+//adding event listener to search button
 searchBtn.addEventListener('click', e=>{
     searchModule.searchRecipe(searchInputEl.value);
 });
 
 
-
+//setting base url for axio requests
 axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1';
+
+//function to load a random recipe 
 async function initialLoad()
 {
     try{
     const response = await axios.get('/random.php');
     let recipe = response.data.meals[0];
     console.log(recipe);
+    //passing the recipe to get it displayed in the browser
     recipeModule.getRecipe(recipe);
 }
 catch (err)
@@ -24,7 +31,7 @@ catch (err)
 }
     
 }
-
+// Calling the initialLoad function to run when the page loads
 initialLoad();
 
 
--- scripts/searchRecipe.js
+++ scripts/searchRecipe.js
@@ -1,23 +1,25 @@
 import { getRecipe } from "./recipe.js";
-
+//function to search for specific recipe and get it displayed
 export async function searchRecipe(searchInputVal) {
 
     if (searchInputVal) {
         try {
             const response = await axios(`/search.php?s=${searchInputVal}`);
             if (response.data.meals) {
+                //calling the getrecipe function from recipe.js file
                 getRecipe(response.data.meals[0]);
             }
             else {
                 alert(`Sorry, We don't have the recipe of ${searchInputVal} now!`)
             }
         }
+        //catch any async request error
         catch (err) {
             console.error('Error fetching data: ', err);
         }
     }
 
     else
-        console.log('Enter a valid search')
+        alert('Enter a valid search');
 
 }
